9d8c30b7a9e78f2e8d0a7b93a28372898588bebc
Felix Pacheco <devfex.19@gmail.com>
2021-09-23 11:36:10 -0400

    Implement Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import logo from 'assets/vectors/complete-logo.svg';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+    width: 100%;
+    height: 153px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+`;
+
+const Footer = (): JSX.Element => (
+    <StyledFooter>
+        <img src={logo} alt="Simple Burger Logo" />
+    </StyledFooter>
+);
+
+export default Footer;
