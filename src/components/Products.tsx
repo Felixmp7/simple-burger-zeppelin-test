3c7180076eda9508f901a3a899e7d3bf83108542
Felix Pacheco <devfex.19@gmail.com>
2021-09-23 11:36:41 -0400

    Refactor Products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,48 +1,21 @@
 import styled from 'styled-components';
 import { IProduct } from 'types';
-import rightFigure from 'assets/vectors/combos-vector-right.svg';
-import leftFigure from 'assets/vectors/combos-vector-left.svg';
 import Title from './Title';
 import Product from './widgets/Product';
 
-interface IStyles {
-    background?: string;
-    bgImage?: string;
-    left?: number | string;
-    right?: number | string;
-}
-
-const Section = styled.section<IStyles>`
-    overflow: hidden;
-    position: relative;
-    /* height: 665.1px; */
-    background-color: ${({ background }) => background};
-`;
-
-const BgFigure = styled.div<IStyles>`
-    height: 100%;
-    width: 50%;
-    background-repeat: repeat-y;
-    background-size: cover;
-    position: absolute;
-    background-position-y: 100%;
-    background-image: ${({ bgImage }) => `url(${bgImage})`};
-    left: ${({ left }) => left};
-    right: ${({ right }) => right};
-`;
-
-const ContainerContent = styled.div`
-    margin: 48.3px auto 0;
+const ContainerContent = styled.div<{ margin?: string}>`
+    margin: ${({ margin }) => margin};
     width: 1200px;
+    padding-top: 40px;
     display: flex;
     flex-direction: column;
     align-items: center;
 
     .products-container {
+        display: grid;
+        grid-template-columns: repeat(4, minmax(0, 1fr));
         margin: 40px 0;
-        display: flex;
         width: 100%;
-        justify-content: space-between;
     }
 `;
 
@@ -50,24 +23,24 @@ type Props = {
     products: Array<IProduct>;
     descriptionTitle: string;
     productTitle: string;
-    backgroundColor: string;
+    margin?: string;
 }
 
 const Products = ({
-    products, descriptionTitle, productTitle, backgroundColor,
+    products, descriptionTitle, productTitle, margin,
 }: Props): JSX.Element => (
-    <Section background={backgroundColor}>
-        <BgFigure left={0} right="auto" bgImage={leftFigure} />
-        <BgFigure right={0} left="auto" bgImage={rightFigure} />
-        <ContainerContent>
-            <Title descriptionTitle={descriptionTitle} productTitle={productTitle} />
-            <div className="products-container">
-                {products.map((product) => (
-                    <Product key={product.id} {...product} />
-                ))}
-            </div>
-        </ContainerContent>
-    </Section>
+    <ContainerContent margin={margin}>
+        <Title descriptionTitle={descriptionTitle} productTitle={productTitle} />
+        <div className="products-container">
+            {products.map((product) => (
+                <Product key={product.id} {...product} />
+            ))}
+        </div>
+    </ContainerContent>
 );
+
+Products.defaultProps = {
+    margin: '48.3px auto',
+};
 
 export default Products;
