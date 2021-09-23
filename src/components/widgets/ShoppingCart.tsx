2d17f8a17eb3947c6daf0d37e266cc612a9e3374
Felix Pacheco <devfex.19@gmail.com>
2021-09-23 11:37:16 -0400

    Import shopCount data from hook

--- src/components/widgets/ShoppingCart.tsx
+++ src/components/widgets/ShoppingCart.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import shopping from 'assets/icons/shop-icon.svg';
 import Price from 'components/Price';
+import useShopList from 'hooks/useShopList';
 
 const Container = styled.div`
     width: 368.66px;
@@ -24,30 +25,32 @@ const ShoppingBar = styled.div`
     align-items: center;
     justify-content: space-between;
     font-weight: 500;
-`;
 
-const ShoppingIcon = styled.img`
-    width: 11.47px;
-    height: 13.28px;
-`;
-
-const Text = styled.span`
-    font-size: 12px;
+    img {
+        width: 11.47px;
+        height: 13.28px;
+    }
+    span {
+        font-size: 12px;
+    }
 `;
 
 type Props = {
     text: string;
-    price: number;
 }
 
-const ShoppingCart = ({ text, price }: Props): JSX.Element => (
-    <Container>
-        <ShoppingBar>
-            <ShoppingIcon src={shopping} alt="Shopping Icon" />
-            <Text>{text}</Text>
-            <Price price={price} />
-        </ShoppingBar>
-    </Container>
-);
+const ShoppingCart = ({ text }: Props): JSX.Element => {
+    const { shopCount } = useShopList();
+
+    return (
+        <Container>
+            <ShoppingBar>
+                <img src={shopping} alt="Shopping Icon" />
+                <span>{text}</span>
+                <Price price={shopCount} />
+            </ShoppingBar>
+        </Container>
+    );
+};
 
 export default ShoppingCart;
