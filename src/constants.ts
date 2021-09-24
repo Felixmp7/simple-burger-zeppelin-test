00ba2a4c8ef492f57153e33ee821918bdc6028d4
Felix Pacheco <devfex.19@gmail.com>
2021-09-24 14:50:46 -0400

    Implement Constants

---
+++ src/constants.ts
@@ -0,0 +1,42 @@
+export const toppings = [
+    { name: 'Mayonnaise' },
+    { name: 'Ketchup' },
+    { name: 'Letucce' },
+    { name: 'Tomato' },
+    { name: 'Pickles' },
+    { name: 'Onion' },
+    { name: 'Bread' },
+];
+export const sizes = [
+    { name: 'Small', extraCost: 0 },
+    { name: 'Big', extraCost: 2.00 },
+];
+export const sodaFlavours = [
+    { name: 'Cola' },
+    { name: 'Lemon' },
+    { name: 'Grapefruit' },
+    { name: 'Water' },
+    { name: 'Orange' },
+];
+
+export const breakPoints = {
+    xs: '320px',
+    sm: '400px',
+    md: '450px',
+    lg: '520px',
+    xl: '620px',
+    tablet: '744px',
+    tablexLg: '',
+    tabletXl: '',
+    laptop: '1080px',
+    laptopLg: '1248px',
+    laptopXl: '1440px',
+    desktop: '1640px',
+};
+
+export const colors = {
+    pink: '#F01C4F',
+    pinkHover: '#ca1841',
+    green: '#5AD88C',
+    greenHover: '#4eb878',
+};
