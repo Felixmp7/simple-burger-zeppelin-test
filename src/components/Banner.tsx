11240a66ff66c3b7b08cd9a174cad47354b2e1f1
Felix Pacheco <devfex.19@gmail.com>
2021-09-24 14:59:33 -0400

    Refactor Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,28 @@
 import styled from 'styled-components';
 import banner from 'assets/banner.png';
 import logo from 'assets/vectors/complete-logo.svg';
-import ShoppingCart from './widgets/ShoppingCart';
+import useShopList from 'hooks/useShopList';
+import ShoppingBar from './widgets/ShoppingBar';
+import { colors } from '../constants';
 
 const Container = styled.div`
     position: relative;
     width: 100%;
     height: 383.7px;
     background: linear-gradient(180deg, rgba(0, 0, 0, 0.76) 0%, rgba(0, 0, 0, 0) 73.31%), url(${banner});
+    background-size: 100%;
     border-radius: 0px 0px 40px 40px;
+
+    .container-total-price {
+        width: 368.66px;
+        height: 72px;
+        position: absolute;
+        top: 0;
+        right: 120px;
+        padding: 16px;
+        background-color: white;
+        border-radius: 0px 0px 10px 10px;
+    }
 `;
 
 const Logo = styled.img`
@@ -17,11 +31,17 @@ const Logo = styled.img`
     left: 104px;
 `;
 
-const Banner = (): JSX.Element => (
-    <Container>
-        <Logo src={logo} alt="Burger Logo" />
-        <ShoppingCart text="View order" />
-    </Container>
-);
+const Banner = (): JSX.Element => {
+    const { shopCount } = useShopList();
+
+    return (
+        <Container>
+            <Logo src={logo} alt="Burger Logo" />
+            <div className="container-total-price">
+                <ShoppingBar text="View order" backgroundColor={colors.pink} price={shopCount} />
+            </div>
+        </Container>
+    );
+};
 
 export default Banner;
