c89684011d98bbf3cf7662d0ef6cc142aa628831
Felix Pacheco <devfex.19@gmail.com>
2021-09-24 14:51:58 -0400

    Export new global types and interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,17 @@
+export type SideDishesProps = {
+    toppings?: Array<{ name: string }>;
+    sodaFlavours?: Array<{ name: string }>;
+    sizes?: Array<{ name: string, extraCost: number | null }>;
+};
+
 export interface IProduct {
     id: number;
     image: string;
     name: string;
     description: string;
+    productType: string;
     price: number;
+    sideDishes: SideDishesProps;
 }
 
 export type Products = {
@@ -11,3 +19,5 @@ export type Products = {
     burgers: Array<IProduct>;
     others: Array<IProduct>;
 };
+
+export interface IProductStyle { image?: string; }
