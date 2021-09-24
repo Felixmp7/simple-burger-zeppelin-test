408b28da2c5ca218206c0aa0d04fc1a7d7257e34
Felix Pacheco <devfex.19@gmail.com>
2021-09-24 15:06:43 -0400

    Fix import path of Product

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { IProduct } from 'types';
 import Title from './Title';
-import Product from './widgets/Product';
+import Product from './Product';
 
 const ContainerContent = styled.div<{ margin?: string}>`
     margin: ${({ margin }) => margin};
