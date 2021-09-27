173c5c8bb5e710483bde8c5b6a11bfdd5c2989dd
Felix Pacheco <devfex.19@gmail.com>
2021-09-27 17:10:00 -0400

    Add new reusable types

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,38 @@
-export type SideDishesProps = {
-    toppings?: Array<{ name: string }>;
-    sodaFlavours?: Array<{ name: string }>;
-    sizes?: Array<{ name: string, extraCost: number | null }>;
+export type CurrentAdditionalProps = {
+    defaultPrice: string;
+    toppings?: Array<string>;
+    size?: SizeProps;
+    sodaFlavour?: string;
+}
+export type SizeProps = {
+    name: string;
+    extraCost: string | null;
+}
+
+export type AdditionalTopicsProps = {
+    toppings?: Array<string> | undefined;
+    sodaFlavour?: string | undefined;
+    size?: SizeProps | undefined;
 };
 
+export interface IProductModalProps extends IProduct {
+    closeModal: () => void;
+}
+
 export interface IProduct {
+    productOrderId?: string | undefined;
     id: number;
     image: string;
     name: string;
     description: string;
     productType: string;
-    price: number;
-    // sideDishes: SideDishesProps;
+    price: string;
+    additionals?: AdditionalTopicsProps | undefined;
 }
 
 export type Products = {
-    combos: Array<IProduct>;
-    burgers: Array<IProduct>;
-    others: Array<IProduct>;
+    products: Array<IProduct>;
+    getProduct: (id: number) => IProduct | undefined;
 };
 
 export interface IProductStyle { image?: string; }
