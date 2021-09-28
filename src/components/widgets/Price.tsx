30aa1293975a6200294350798be74a972707810c
Felix Pacheco <devfex.19@gmail.com>
2021-09-28 17:56:21 -0400

    Add prop to set dollar color in price widget

--- src/components/widgets/Price.tsx
+++ src/components/widgets/Price.tsx
@@ -1,18 +1,25 @@
 import styled from 'styled-components';
 
-const Container = styled.span`
+const Container = styled.span<{ dollarColor?: string }>`
     font-size: 12px;
 
     &:before {
         content: '$';
-        color: #ffffffaa;
+        color: ${({ dollarColor }) => dollarColor};
     }
 `;
 
-type Props = { price: string | number };
+type Props = {
+    price: string | number;
+    dollarColor?: string;
+};
 
-const Price = ({ price }: Props): JSX.Element => (
-    <Container>{price}</Container>
+const Price = ({ price, dollarColor }: Props): JSX.Element => (
+    <Container dollarColor={dollarColor}>{price}</Container>
 );
+
+Price.defaultProps = {
+    dollarColor: '#ffffffaa',
+};
 
 export default Price;
