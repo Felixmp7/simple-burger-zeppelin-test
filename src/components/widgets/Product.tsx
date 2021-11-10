e9497f6e56e7520dc7c8f9147b1a73e2dda5eff8
Felix Pacheco <devfex.19@gmail.com>
2021-11-10 13:13:17 -0400

    Clean spaces

--- src/components/widgets/Product.tsx
+++ src/components/widgets/Product.tsx
@@ -8,20 +8,17 @@ import Price from './Price';
 import CartButton from './CartButton';
 
 const Product: FC<IProduct> = (props) => {
-    const [addNewProduct, setAddNewProduct] = useState(false);
-    useDisableBodyScroll(addNewProduct);
+    const [isAddNew, setIsAddNew] = useState(false);
+    useDisableBodyScroll(isAddNew);
     const {
         image, name, description, price,
     } = props;
 
     return (
         <Container>
-            {addNewProduct && (
+            {isAddNew && (
                 <ScreenModal>
-                    <AddNewProduct
-                        {...props}
-                        closeModal={() => setAddNewProduct(false)}
-                    />
+                    <AddNewProduct closeModal={() => setIsAddNew(false)} {...props} />
                 </ScreenModal>
             )}
             <Image image={image} />
@@ -31,9 +28,7 @@ const Product: FC<IProduct> = (props) => {
                 <div className="container-actions">
                     <div className="bubble"><Price dollarColor="#00000055" price={price} /></div>
                     <div className="container-cart-button">
-                        <CartButton
-                            handleClick={() => setAddNewProduct(true)}
-                        />
+                        <CartButton handleClick={() => setIsAddNew(true)} />
                     </div>
                 </div>
             </div>
