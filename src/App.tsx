2d0b7f08879525d335216255d3c0a018b0094c81
Felix Pacheco <devfex.19@gmail.com>
2021-11-10 08:14:07 -0400

    Install spinners-react packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import Banner from 'components/Banner';
 import Footer from 'components/Footer';
 import Products from 'components/Products';
 import SectionBg from 'components/SectionBg';
-import ScreenMessage from 'components/widgets/ScreenMessage';
+import ScreenLoader from 'components/widgets/ScreenLoader';
 import useProducts from 'hooks/useProducts';
 
 const App = (): JSX.Element => {
     const { isLoading, products } = useProducts();
 
     if (isLoading) {
-        return <ScreenMessage message="Loading..." />;
+        return <ScreenLoader />;
     }
     return (
         <>
