5adb2b21755c25505c69474e50db425b7ccf92fd
Felix Pacheco <devfex.19@gmail.com>
2021-11-17 08:10:43 -0400

    Import DeliveryTime widget and redirect if are not products in shoppingCart

--- src/components/OrderBoard.tsx
+++ src/components/OrderBoard.tsx
@@ -1,8 +1,10 @@
+import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import { DragDropContext } from 'react-beautiful-dnd';
 import useDNDProducts from 'hooks/useDNDProducts';
 import BoardColumn from './BoardColumn';
 import { doneStatus, inProcessStatus, pendingStatus } from '../constants';
+import DeliveryTime from './widgets/DeliveryTime';
 
 const Container = styled.div`
     .container-grid {
@@ -10,6 +12,13 @@ const Container = styled.div`
         display: grid;
         grid-template-columns: repeat(3, minmax(0, 1fr));
         gap: 3rem;
+    }
+
+    .header-content {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 1rem;
     }
 `;
 
@@ -22,9 +31,16 @@ const OrderBoard = (): JSX.Element => {
         }, onDragEnd,
     } = useDNDProducts();
 
+    if (!pendings.length && !inProcess.length && !done.length) {
+        return <Redirect to="/" />;
+    }
+
     return (
         <Container>
-            <h2>Orders</h2>
+            <div className="header-content">
+                <h2>Order</h2>
+                <DeliveryTime />
+            </div>
             <DragDropContext onDragEnd={onDragEnd}>
                 <div className="container-grid">
                     <BoardColumn
