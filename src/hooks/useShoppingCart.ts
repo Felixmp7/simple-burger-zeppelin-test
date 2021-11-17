bc8c7b90738894f50a97cd076dcd0199594dd4e8
Felix Pacheco <devfex.19@gmail.com>
2021-11-17 08:09:37 -0400

    Export new methods of useShoppingCart

--- src/hooks/useShoppingCart.ts
+++ src/hooks/useShoppingCart.ts
@@ -3,6 +3,7 @@ import { SetterOrUpdater, useRecoilState } from 'recoil';
 import produce from 'immer';
 import shopListAtom from 'recoilState';
 import { IProduct, SizeProps } from 'types';
+import { doneStatus } from '../constants';
 
 interface IUpdateProps { productOrderId?: string; }
 
@@ -11,6 +12,8 @@ interface IUpdateFlavourProps extends IUpdateProps { newFlavour: string; }
 interface IUpdateSizeProps extends IUpdateProps { newSize: SizeProps; }
 
 interface IUpdateToppingsProps extends IUpdateProps { topping: string; }
+
+interface IUpdateStatusProps extends IUpdateProps { status: string; }
 
 interface IProps extends IProduct {
     callback: () => void;
@@ -21,6 +24,7 @@ interface IProps extends IProduct {
 
 interface IShoppingCartHookProps {
     shopList: Array<IProduct>;
+    deliveryTime: number;
     getTotalPrice: () => string;
     setShopList: SetterOrUpdater<Array<IProduct>>;
     handleAddOrRemove: (product: IProduct) => void;
@@ -29,6 +33,7 @@ interface IShoppingCartHookProps {
     updateToppings: ({ productOrderId, topping }: IUpdateToppingsProps) => void;
     getProductSubtotal: (productOrderId: string) => string;
     addNewProduct: (params: IProps) => void;
+    updateStatus: ({ productOrderId, status }: IUpdateStatusProps) => void;
 }
 
 const useShoppingCart = (): IShoppingCartHookProps => {
@@ -124,9 +129,22 @@ const useShoppingCart = (): IShoppingCartHookProps => {
 
         setShopList(shopListUpdated);
     };
+    const updateStatus = ({ productOrderId, status } : IUpdateStatusProps) => {
+        const shopListUpdated = produce(shopList, (draft) => {
+            const index = draft.findIndex((item) => item.productOrderId === productOrderId);
+            if (index !== -1) {
+                draft[index].status = status;
+            }
+        });
+
+        setShopList(shopListUpdated);
+    };
+
+    const getDeliveryTime = () => shopList.reduce((totalAccum, { deliveryTime, status }) => (status === doneStatus ? totalAccum : totalAccum + deliveryTime), 0);
 
     return {
         shopList,
+        deliveryTime: getDeliveryTime(),
         addNewProduct,
         getTotalPrice,
         setShopList,
@@ -134,6 +152,7 @@ const useShoppingCart = (): IShoppingCartHookProps => {
         updateProductSize,
         updateSodaFlavour,
         updateToppings,
+        updateStatus,
         getProductSubtotal,
     };
 };
